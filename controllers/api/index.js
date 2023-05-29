73f6de35bb5b81e5f0a73948fdc86e8b0ae381a5
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-05-28 21:10:20 -0400

    so far the database is working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes.js');
-const postRoutes = require('../later/postRoutes.js');
-const commentRoutes = require('../later/commentRoutes.js');
+const postRoutes = require('./postRoutes.js');
+const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
@@ -10,4 +10,4 @@ router.use('/comments', commentRoutes);
 
 module.exports = router;
 
-//This file we have userRoutes, postRoutes and commentRoutes (whose pathways updated when I moved the files, apparently).  These files will be used to determine which routes are viewing end points on the handlebars.
+//This file we have userRoutes, postRoutes and commentRoutes.  These files will be used to determine which routes are viewing end points on the handlebars.
